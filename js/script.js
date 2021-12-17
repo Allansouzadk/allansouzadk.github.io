8a801c42cef62f244e68945268df7600e8e2aecf
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-12-17 08:57:21 -0300

    initial navigation on a page

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelector("#rd_home").addEventListener("click", function () {
+    document.querySelectorAll(".nav label")[0].style.color = "#000";
+    document.querySelectorAll(".nav label")[1].style.color = "#000";
+});
+
+document.querySelector("#rd_contact").addEventListener("click", function () {
+    document.querySelectorAll(".nav label")[0].style.color = "#fff";
+    document.querySelectorAll(".nav label")[1].style.color = "#fff";
+});
